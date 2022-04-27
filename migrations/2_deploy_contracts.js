9fee2b285a493ae5feb124c26860f99023dce8bf
Bigjasn <jasoliu.biz@gmail.com>
2022-04-27 06:50:50 -0400

    add market

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,7 @@
 var GameItems = artifacts.require("./GameItems.sol");
+var ItemMarket = artifacts.require("./ItemMarket.sol");
 
 module.exports = deployer => {
   deployer.deploy(GameItems);
+  deployer.deploy(ItemMarket);
 };
